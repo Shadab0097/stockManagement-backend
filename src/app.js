cdfc9db24f4ae66b5ad6f8a9871ac848becbb723
shadab khan <shadabkhan2910@gmail.com>
2025-03-20 14:28:35 +0530

    vercel updated

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 require("dotenv").config();
 
 app.use(cors({
-    origin: ' http://localhost:5173/',
+    origin: ' http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
