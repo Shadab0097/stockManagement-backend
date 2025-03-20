5291272cd659b3b53f20c4143c5c0fe8654d74a7
shadab khan <shadabkhan2910@gmail.com>
2025-03-20 13:35:29 +0530

    headers cors

--- src/app.js
+++ src/app.js
@@ -6,19 +6,22 @@ const cors = require('cors');
 
 require("dotenv").config();
 
-// app.use(cors({
-//     origin: '"https://stock-management-black.vercel.app/',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: '"https://stock-management-black.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization'
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://stock-management-black.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+}));
+
+// app.use(
+//     cors({
+//         origin: 'https://stock-management-black.vercel.app', // Frontend URL
+//         credentials: true, // Allow cookies and credentials
+//         methods: 'GET,POST,PUT,DELETE,OPTIONS',
+//         allowedHeaders: 'Content-Type,Authorization'
+//     })
+// );
 app.use(express.json());
 app.use(cookieParser());
 
