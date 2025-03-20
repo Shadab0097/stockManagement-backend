89b51dfd89f9d842466204f856a1e617d9a6e9dd
shadab khan <shadabkhan2910@gmail.com>
2025-03-20 15:19:01 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -12,11 +12,9 @@ app.use(cors({
 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-
 }));
 
-app.options('*', cors());
+
 
 
 app.use(express.json());
