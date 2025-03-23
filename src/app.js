a782da7100ca6c44f117ea34f7b21b26ad7afa35
shadab khan <shadabkhan2910@gmail.com>
2025-03-23 13:20:24 +0530

    allowed header

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(cors({
     origin: 'https://stock-management-kappa-ten.vercel.app',
 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type ', 'Authorization'],
     credentials: true,
 }));
 
